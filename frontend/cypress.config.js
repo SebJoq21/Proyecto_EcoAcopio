8531f08ee2aee2ed96a9570417e8d058e4bd1a57
achliii <ashley.bayonav@gmail.com>
2026-06-12 11:39:52 -0500

    chore(frontend): setup cypress testing dependencies and configuration

---
+++ frontend/cypress.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  e2e: {
+    baseUrl: "http://localhost:5173",
+    supportFile: "cypress/support/e2e.js",
+    viewportWidth: 1280,
+    viewportHeight: 720,
+    video: false,
+    screenshotOnRunFailure: false,
+  },
+});
